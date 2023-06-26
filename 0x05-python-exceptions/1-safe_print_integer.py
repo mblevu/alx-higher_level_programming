41890c81b91d8c66d98b8d443d8cf8e9ef323fed
levu <levu@kali>
2023-06-26 23:13:46 +0300

    printinglist integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
+        integer_value = int(value)
+        print("{:d}".format(integer_value))
         return (True)
     except (ValueError,TypeError):
         return (False)
