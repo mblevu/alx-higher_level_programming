f5ba17d79527a6acd6d053368be5d9e8288fbd01
levu <levu@kali>
2023-07-03 18:03:32 +0300

    creating rectangle class

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number. Default is 98.
+
+    Returns:
+        int: The sum of the two numbers.
+
+    Raises:
+        TypeError: If either `a` or `b` is not an integer.
+    """
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    return int(a) + int(b)
--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module:0-rectangle
+empty class that defines a rectangle
+"""
 class Rectangle:
     """
     This class represents a rectangle
