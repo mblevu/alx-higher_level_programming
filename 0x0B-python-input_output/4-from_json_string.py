e9c3fdd937c1a1cd1fd76594d85b3a85c3171d62
levu <levu@kali>
2023-07-12 04:07:18 +0300

    completing python io

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 # 6-from_json_string.py
+"""Defines a JSON-to-object function."""
 import json
-
-"""
-returns an object (Python data structure)
-represented by a JSON string
-"""
 
 
 def from_json_string(my_str):
