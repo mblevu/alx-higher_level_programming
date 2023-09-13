fcc2db6972ad66f6e08bc07cc09859b6d121f2b5
kintokeanu <levu@kali>
2023-09-13 21:48:07 +0300

    update

--- 0x12-javascript-warm_up/0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x12-javascript-warm_up/0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  function convertToBase (number) {
-    if (number < base) {
-      return number.toString(base);
-    } else {
-      return convertToBase(Math.floor(number / base)) + (number % base).toString();
-    }
-  }
-
-  return convertToBase;
+  return function (num) {
+    return num.toString(base);
+  };
 };
