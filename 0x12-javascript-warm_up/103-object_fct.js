8f3f9a38c46a7cca6e0c12d7242e895c64fa834a
kintokeanu <levu@kali>
2023-09-13 21:35:49 +0300

    adding incr function

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,12 +2,13 @@
 
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
 };
-
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
