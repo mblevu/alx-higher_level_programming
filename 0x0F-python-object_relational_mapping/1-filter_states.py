e670ffb05b2c5e2d794675dcc6663960a49dba97
kintokeanu <levu@kali>
2023-09-19 11:33:51 +0300

    selecting states starting with N order by id

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,17 +5,17 @@ import sys
 
 
 if __name__ == "__main__":
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=sys.argv[1], passwd=sys.argv[2],
-                             db=sys.argv[3])
-        cursor = db.cursor()
 
-        cursor.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ")
-        states = cursor.fetchall()
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
 
-        for state in states:
-            print(state)
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ")
+    states = cursor.fetchall()
 
-        cursor.close()
-        db.close()
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
