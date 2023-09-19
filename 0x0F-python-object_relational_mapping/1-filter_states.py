b44ce026c2650514444e2f2c398bbdd56825fc66
kintokeanu <levu@kali>
2023-09-19 12:04:35 +0300

    selecting states starting with N order by id

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ")
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id"
+    )
     rows = cursor.fetchall()
 
     for row in rows:
