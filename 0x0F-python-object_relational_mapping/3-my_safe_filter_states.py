2a46c6b74a3fe46996a2d769790f6740d3777659
kintokeanu <levu@kali>
2023-09-21 23:59:00 +0300

    filtering states and cities

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], portf=3306)
+    cursor = db.cursor()
+    match = sys.argv[4]
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (match,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
