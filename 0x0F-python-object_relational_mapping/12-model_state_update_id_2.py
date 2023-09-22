275b0839e6e37e3ad89674d9f328bc2bcc10bc03
kintokeanu <levu@kali>
2023-09-22 23:29:36 +0300

    connecting with sqlalchemy

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,6 @@
-#!usr/bin/python3
-"""changes the name of a state object as argument from database"""
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
