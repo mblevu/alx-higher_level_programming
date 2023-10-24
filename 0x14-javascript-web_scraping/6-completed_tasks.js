c4389a96efdc1c2c90ca7fcf880e3ec4a6c8d4b3
kintokeanu <brianislevu@gmail.com>
2023-10-25 01:29:58 +0300

    computing number of tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const tasks = JSON.parse(body);
+
+  const completedTasks = {};
+
+  tasks.forEach((task) => {
+    if (task.completed) {
+      if (completedTasks[task.userId]) {
+        completedTasks[task.userId]++;
+      } else {
+        completedTasks[task.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasks);
+});
