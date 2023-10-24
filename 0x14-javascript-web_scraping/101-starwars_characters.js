46d5a2a44ad31465f7ae0f691b49a314467c2e85
kintokeanu <brianislevu@gmail.com>
2023-10-25 01:44:14 +0300

    printing character of starwars

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
